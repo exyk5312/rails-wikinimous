0c5a94b6812c8939cf616d181a9abde7cbeba1cc
Eryk Alexandre Doucet <eadoucet@gmail.com>
2020-11-10 15:55:42 -0500

    created index and show page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,29 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def create
+    Article.new(@article)
+  end
+
+  def new
+
+  end
+
+  def edit
+
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+  
+  def update
+
+  end
+
+  def destroy
+
+  end
 end
